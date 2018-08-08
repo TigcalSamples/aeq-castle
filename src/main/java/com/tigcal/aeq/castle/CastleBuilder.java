544743857b3bafe80c847254c6ad474d83895685
Jomar Tigcal <jomar@tigcal.com>
2018-08-08 11:01:01 -0700

    Initial project structure
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

---
+++ src/main/java/com/tigcal/aeq/castle/CastleBuilder.java
@@ -0,0 +1,10 @@
+package com.tigcal.aeq.castle;
+
+public class CastleBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/tigcal/aeq/castle/CastleBuilderTest.java
@@ -0,0 +1,14 @@
+package com.tigcal.aeq.castle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CastleBuilderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
